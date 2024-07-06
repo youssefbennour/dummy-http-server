18412717fdbfbe8e9b04334e7a8c3ae6d8ac7c41
youssefbennour <youssefbennour15@gmail.com>
2024-07-06 12:06:34 +0100

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -6,7 +6,18 @@ TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 Socket socket = server.AcceptSocket(); // wait for client
 
-string response = "HTTP/1.1 200 OK\r\n\r\n";
-byte[] bytesResponse = Encoding.ASCII.GetBytes(response);
+var buffer = new byte[256];
+await socket.ReceiveAsync(buffer);
+
+var request = Encoding.UTF8.GetString(buffer);
+var path = request.Split("\r\n")
+    .FirstOrDefault()?
+    .Split(" ")[1];
+
+var response = path is "/" 
+    ? "HTTP/1.1 200 OK\r\n\r\n" 
+    : "HTTP/1.1 404 Not Found\r\n\r\n";
+
+var bytesResponse = Encoding.ASCII.GetBytes(response);
 socket.Send(bytesResponse);
 
