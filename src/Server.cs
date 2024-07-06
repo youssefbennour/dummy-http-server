4029f4c0232a81a4adb9fbe64c81c781549b122e
youssefbennour <youssefbennour15@gmail.com>
2024-07-06 11:55:27 +0100

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,12 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+Socket socket = server.AcceptSocket(); // wait for client
+
+string response = "HTTP/1.1 200 OK\r\n\r\n";
+byte[] bytesResponse = Encoding.ASCII.GetBytes(response);
+socket.Send(bytesResponse);
+
